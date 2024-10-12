cdb287446eac9f6a8e6625cd723855f7c52fa200
Mikołaj Paterek <82290162+P1aster@users.noreply.github.com>
2024-10-12 19:24:37 +0200

    tsp main

--- PEA_TSP/main.cpp
+++ PEA_TSP/main.cpp
@@ -3,10 +3,15 @@
 #include "BF.h"
 #include "RNN.h"
 #include "ConfigReader.h"
+#include "Time.h"
 
 int main(int argslen, char* args[]) {
     Graph graph;
+    Time timer;
+    TSP_Result result;
     std::string inputPath;
+    std::string outputPath;
+    bool isFileOpended = false;
 
 
     if (argslen == 0) {
@@ -22,7 +27,7 @@ int main(int argslen, char* args[]) {
 
     if (configReader.checkPathIsFile(inputPath)) {
         if (graph.loadFromFile(inputPath)) {
-            std::cout << "Loaded graph from file: " << inputPath << std::endl;
+            std::cout << "Loaded graph from file: " << inputPath << "\n\n";
             graph.printMatrix();
         }
         else {
@@ -30,50 +35,69 @@ int main(int argslen, char* args[]) {
             return 1;
         }
 
-        std::cout << std::endl << "Brute force algorithm:" << "\n\n";
+        isFileOpended = configReader.openOutputFile();
+        if (!isFileOpended) {
+            std::cerr << "Failed to open output file: " << outputPath << std::endl;
+            return 1;
+        }
 
         BF bf(graph);
-        RNN rnn(graph);
 
-        TSP_Result resultRNN = rnn.findRepeatedNearestNaighbour();
-        TSP_Result resultNN = rnn.findNearestNaighbour();
-        TSP_Result resultBF = bf.findCheapestHamiltonianCircle();
+        configReader.writeToOutputFile("Instance,Brute force algorithm\n");
+        configReader.writeToOutputFile("Input Path," + inputPath + "\n");
+        configReader.writeToOutputFile("Nodes Number," + std::to_string(graph.getNodesNumber()) + "\n");
+        configReader.writeToOutputFile("Time,[ms]\n");
 
-
-        std::cout << "Best path RNN: " << resultRNN.minPathCost << "   ";
-        for (size_t i = 0; i < resultRNN.bestPath.size(); ++i) {
-            std::cout << resultRNN.bestPath[i] << " ";
+        for (int i = 0; i < configReader.getRepeatNumber(); i++) {
+            timer.start();
+            result = bf.findCheapestHamiltonianCircle();
+            timer.stop();
+            configReader.writeToOutputFile(std::to_string(i+1)+ "," + std::to_string(timer.getElapsedTime()) + "\n");
         }
-        std::cout << "\n\n\n";
 
-        std::cout << "Best path NN: " << resultNN.minPathCost << "   ";
-        for (size_t i = 0; i < resultNN.bestPath.size(); ++i) {
-            std::cout << resultNN.bestPath[i] << " ";
-        }
-        std::cout << "\n\n\n";
-
-        std::cout << "Best path BF: " << resultBF.minPathCost << "   ";
-        for (size_t i = 0; i < resultBF.bestPath.size(); ++i) {
-            std::cout << resultBF.bestPath[i] << " ";
-        }
-        std::cout << "\n\n\n";
-
+        std::cout << std::endl;
     }
     else {
+
+        isFileOpended = configReader.openOutputFile();
+        if (!isFileOpended) {
+            std::cerr << "Failed to open output file: " << outputPath << std::endl;
+            return 1;
+        }
+
+
+        configReader.writeToOutputFile("Instance,Brute force algorithm\n");
+
+
         for (const auto& entry : std::filesystem::directory_iterator(inputPath)) {
             if (entry.is_regular_file()) {
                 std::string filePath = entry.path().string();
                 std::cout << "Processing file: " << filePath << std::endl;
                 if (graph.loadFromFile(filePath)) {
+                    std::cout << "Loaded graph from file: " << filePath << "\n\n";
                     graph.printMatrix();
+                    std::cout << std::endl;
                 }
                 else {
                     std::cerr << "Failed to load graph from file: " << filePath << std::endl;
+                    return 1;
                 }
+                BF bf(graph);
+
+                configReader.writeToOutputFile("Input Path," + filePath + "\n");
+                configReader.writeToOutputFile("Nodes Number," + std::to_string(graph.getNodesNumber()) + "\n");
+                configReader.writeToOutputFile("Time,[ms]\n");
+
+                for (int i = 0; i < configReader.getRepeatNumber(); i++) {
+                    timer.start();
+                    result = bf.findCheapestHamiltonianCircle();
+                    timer.stop();
+                    configReader.writeToOutputFile(std::to_string(i + 1) + "," + std::to_string(timer.getElapsedTime()) + "\n");
+                }
+
+                configReader.writeToOutputFile("\n\n");
             }
         }
     }
-
-    
     return 0;
 }
